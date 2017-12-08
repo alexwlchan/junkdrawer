e6851fcefbc5bb1bacb63ed659f58f22e9fde107
Alex Chan <alex@alexwlchan.net>
2017-12-08 20:48:31 +0000

    Add a Make task for installing my fish config

--- Makefile
+++ Makefile
@@ -3,3 +3,7 @@ ROOT = $(shell git rev-parse --show-toplevel)
 include safari-stylesheet/Makefile
 
 safari-stylesheet/style.css: $(ROOT)/safari-stylesheet/style.css
+
+~/.config/fish/config.fish:
+	mkdir -p ~/.config/fish
+	echo ". $(ROOT)/fishconfig/config.fish" >> ~/.config/fish/config.fish
--- fishconfig/config.fish
+++ fishconfig/config.fish
@@ -38,7 +38,7 @@ alias cdir 'for l in (ls); if [ -d $l ]; echo (find $l | wc -l)"  $l"; end; end 
 # virtualfish -- a fish wrapper for virtualenv
 # https://github.com/adambrenecki/virtualfish
 ###############################################################################
-eval (python -m virtualfish auto_activation) >> /dev/null 2>&1
+eval (python2 -m virtualfish auto_activation) >> /dev/null 2>&1
 
 
 ###############################################################################
