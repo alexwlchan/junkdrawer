a86b0cdccd6dcc1b5ea531bc4df57d33f60342bf
Alex Chan <alex@alexwlchan.net>
2017-12-23 22:07:17 +0000

    Make it easier to reuse that code

--- fishconfig/dockerfiles.fish
+++ fishconfig/dockerfiles.fish
@@ -1,7 +1,12 @@
-function lessc
+function __call_docker_image
   pushd $ROOT
-    make $ROOT/.docker/lessc >/dev/null
+    make $ROOT/.docker/$argv[1] >/dev/null
   popd
 
-  docker run --rm --tty --volume (pwd):/data alexwlchan/lessc "$argv"
+  docker run --rm --tty --volume (pwd):/data alexwlchan/$argv[1] "$argv[2..-1]"
+end
+
+
+function lessc
+  __call_docker_image lessc
 end
