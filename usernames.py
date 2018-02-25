73aae17027e02d251e276baaafe3132c75aca954
Alex Chan <alex@alexwlchan.net>
2018-02-25 22:14:55 +0000

    usernames.py: better comments and docstring

--- usernames.py
+++ usernames.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8
+"""
+On sites where I don't want to use my standard username (@alexwlchan) --
+for example, if I'm starring content but not creating anything -- I create
+alliterative usernames from the names provided by Docker.
+
+e.g. on GitHub I might use "goofy_galileo"
+
+This script generates an alliterative username for me.
+
+Usage: pass a single letter as first argument, and it offers five suggestions:
+
+    $ ./usernames.py a
+    angry_almeida
+    admiring_ardinghelli
+    admiring_austin
+    amazing_aryabhata
+    admiring_albattani
+
+"""
 
 import os
 import random
@@ -20,7 +39,30 @@ def main():
 
     go_src = open('names-generator.go').read()
 
+    # The adjectives are an array of strings:
+    #
+    #       var (
+    #           left = [...]string{
+    #               "admiring",
+    #               "adoring",
+    #               ...
+    #               "zen",
+    #           }
+    #
     adjectives_src = go_src.split('left = [...]string{')[1].split('}')[0]
+
+    # The names are another array of strings, but with comments on names
+    # to explain their significance:
+    #
+    #           right = [...]string{
+    #               // Muhammad ibn Jābir ...
+    #               "albattani",
+    #
+    #               ...
+    #
+    #               "zhukovsky",
+    #           }
+    #
     names_src = go_src.split('right = [...]string{')[1].split('\n)')[0]
 
     adjectives = [
