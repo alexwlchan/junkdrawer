ef686718160368cd89881421f9294655b50e6488
Alex Chan <alex@alexwlchan.net>
2018-03-03 11:55:46 +0000

    backup_twitter.py: implement the first get_tweets()

--- backup_twitter/backup_twitter.py
+++ backup_twitter/backup_twitter.py
@@ -18,6 +18,7 @@ Options:
 import attr
 import docopt
 import hcl
+import tenacity
 import tweepy
 
 
@@ -50,8 +51,40 @@ def setup_api(credentials):
     return tweepy.API(auth)
 
 
+def get_tweets(method, *args, **kwargs):
+    """Generates tweets for a given API method.
+
+    :param method: The tweepy API method to call.
+    :param args: args to pass to the API method.
+    :param kwargs: kwargs to pass to the API method.
+
+    """
+    # We always want the extended mode: this includes the full text of tweets
+    # that are >140 characters.
+    # https://dev.twitter.com/overview/api/upcoming-changes-to-tweets
+    kwargs['tweet_mode'] = 'extended'
+
+    @tenacity.retry(wait=tenacity.wait_exponential(multiplier=1, max=10))
+    def _get_next_tweets():
+        return method(*args, **kwargs)
+
+    # Keep going until we've exhausted all the tweets from the API, or
+    # something else causes us to break.
+    while True:
+        new_tweets = _get_next_tweets()
+        yield from new_tweets
+
+        # What is the earliest ID of the tweets we've seen?  We'll want
+        # to get everything up to that point on the next call.
+        earliest_id = min(tweet.id for tweet in new_tweets)
+        kwargs['max_id'] = earliest_id - 1
+
+
 if __name__ == '__main__':
     args = docopt.docopt(__doc__)
 
     credentials = TwitterCredentials.from_path(args['--credentials'])
     api = setup_api(credentials=credentials)
+
+    for t in get_tweets(api.user_timeline):
+        print(t)
