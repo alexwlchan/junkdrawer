34d074601cb5889fde69383b83930f45eb4363e1
Alex Chan <alex@alexwlchan.net>
2018-03-03 11:26:18 +0000

    backup_twitter: define the interface

---
+++ backup_twitter/backup_twitter.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+Usage: backup_twitter.py --dir=<DIR> --method=(user_timeline|mentions|favorites) --auth=<AUTH_FILE>
+       backup_twitter.py -h --help
+"""
