0059192385eee0847937511f70d066f8ca715e68
Alex Chan <a.chan@wellcome.ac.uk>
2018-05-18 13:08:30 +0100

    Better error handling in the AWS shortcuts

--- fishconfig/_aws.fish
+++ fishconfig/_aws.fish
@@ -4,13 +4,16 @@ function s3mate
 
   pushd (mktemp -d)
     aws s3 cp "$s3key" "$localname"
-    cp "$localname" "$localname.copy"
-    mate -w "$localname"
 
-    # If the files have changed, upload the new version back to S3.
-    cmp "$localname" "$localname.copy" >/dev/null
-    if [ "$status" != "0" ]
-      aws s3 cp "$localname" "$s3key"
+    if [ "$status" = "0" ]
+      cp "$localname" "$localname.copy"
+      mate -w "$localname"
+
+      # If the files have changed, upload the new version back to S3.
+      cmp "$localname" "$localname.copy" >/dev/null
+      if [ "$status" != "0" ]
+        aws s3 cp "$localname" "$s3key"
+      end
     end
   popd
 end
@@ -21,6 +24,9 @@ function s3cat
 
   pushd (mktemp -d)
     aws s3 cp "$s3key" "$localname"
-    cat "$localname"
+
+    if [ "$status" = "0" ]
+      cat "$localname"
+    end
   popd
 end
