81b8b29abd8ad65b27a2cc2dd6bcb368afbfc7c9
Alex Chan <a.chan@wellcome.ac.uk>
2018-05-18 13:10:53 +0100

    Add a little helper for doing Keybase decryptions

--- fishconfig/_wellcome.fish
+++ fishconfig/_wellcome.fish
@@ -4,3 +4,20 @@ function sqs_redrive
     --src="https://sqs.eu-west-1.amazonaws.com/760097843905/"$queue_name"_dlq" \
     --dst="https://sqs.eu-west-1.amazonaws.com/760097843905/"$queue_name
 end
+
+
+# Uses Keybase to decrypt a string encrypted with my PGP key.
+#
+# This adds the necessary PGP headers, which saves me a small amount
+# of hassle each time.
+#
+function keybase_decrypt
+  pushd (mktemp -d)
+    echo "-----BEGIN PGP MESSAGE-----"  > encrypted.txt
+    echo ""                            >> encrypted.txt
+    pbpaste                            >> encrypted.txt
+    echo "-----END PGP MESSAGE-----"   >> encrypted.txt
+
+    keybase decrypt -i encrypted.txt
+  popd
+end
