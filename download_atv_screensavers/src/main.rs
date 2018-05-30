63af8b3c71ffd392645e9f30d47716b0ca18be5c
Alex Chan <alex@alexwlchan.net>
2018-05-30 22:10:28 +0100

    Parse the JSON, get all the assets + names

--- download_atv_screensavers/src/main.rs
+++ download_atv_screensavers/src/main.rs
@@ -1,9 +1,52 @@
 extern crate reqwest;
+extern crate serde;
+extern crate serde_json;
 
-fn main() {
-    let text = reqwest::get("https://www.rust-lang.org")
-        .unwrap()
-        .text()
-        .unwrap();
-    println!("text = {:?}", text);
+#[macro_use]
+extern crate serde_derive;
+
+
+use std::path::Path;
+
+
+#[derive(Deserialize, Debug)]
+struct Asset {
+    url: String,
+    #[serde(rename = "accessibilityLabel")] accessibility_label: String,
+    #[serde(rename = "type")] asset_type: String,
+    id: String,
+    #[serde(rename = "timeOfDay")] time_of_day: String
+}
+
+
+#[derive(Deserialize, Debug)]
+struct AssetCollection {
+    id: String,
+    assets: Vec<Asset>
+}
+
+
+fn main() -> Result<(), serde_json::Error> {
+    let download_url = "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/entries.json";
+
+    let text = reqwest::get(download_url).unwrap()
+        .text().unwrap();
+
+    let all_collections: Vec<AssetCollection> = serde_json::from_str(&text)?;
+
+    let all_assets = all_collections.iter()
+        .map(|collection| collection.assets.iter())
+        .flat_map(|it| it.clone());;
+
+    for (i, asset) in all_assets.enumerate() {
+        let extension = Path::new(&asset.url)
+            .extension()
+            .unwrap()
+            .to_str()
+            .unwrap();
+        let name = format!("{:03}-{}-{}.{}", i, asset.accessibility_label, asset.time_of_day, extension);
+        println!("The name is {:?}", name);
+    }
+
+    Ok(())
 }
