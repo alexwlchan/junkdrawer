7eb9e103a0c90ae086fd6acebc9943b729fc1392
Alex Chan <alex@alexwlchan.net>
2018-06-06 00:05:56 +0100

    backup_twitter.sh works from wherever it's called in the file system

--- backup_twitter/backup_twitter.sh
+++ backup_twitter/backup_twitter.sh
@@ -8,21 +8,21 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo ""
 echo "*** user_timeline"
-python backup_twitter.py \
+python $DIR/backup_twitter.py \
   --credentials=$DIR/auth.json \
   --dir=/Users/alexwlchan/Dropbox/twitter/user_timeline \
   --method=user_timeline
 
 echo ""
 echo "*** mentions"
-python backup_twitter.py \
+python $DIR/backup_twitter.py \
   --credentials=$DIR/auth.json \
   --dir=/Users/alexwlchan/Dropbox/twitter/mentions \
   --method=mentions_timeline
 
 echo ""
 echo "*** favorites"
-python backup_twitter.py \
+python $DIR/backup_twitter.py \
   --credentials=$DIR/auth.json \
   --dir=/Users/alexwlchan/Dropbox/twitter/favorites \
   --method=favorites
