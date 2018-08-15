745c8f7ed3e7c837ae617fd460c349a15f7dcaf0
Alex Chan <a.chan@wellcome.ac.uk>
2018-08-15 15:13:47 +0100

    Add a script for changing a transformer DLQ to source data entries

---
+++ wellcome/transformer_dlq_to_sourcedata.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8
+"""
+Usage: transformer_dlq_to_sourcedata.py <PATH>
+"""
+
+import json
+
+import boto3
+import docopt
+
+
+if __name__ == '__main__':
+    args = docopt.docopt(__doc__)
+    path = args['<PATH>']
+
+    jl = json.loads
+
+    s3 = boto3.client('s3')
+
+    for line in open(path):
+        message = jl(jl(jl(line)['Body'])['Message'])
+
+        if message['sourceName'] == 'sierra':
+            bucket = 'wellcomecollection-vhs-sourcedata-sierra'
+        else:
+            assert False, message
+
+        body = s3.get_object(Bucket=bucket, Key=message['s3key'])['Body']
+        contents = body.read()
+        print(contents)
