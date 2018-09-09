6026cf318c2eba8f454d93044d9b1b8221315c98
Alex Chan <alex@alexwlchan.net>
2018-09-09 14:56:41 +0100

    Allow running save_single_tweet.py from any directory

--- backup_twitter/save_single_tweet.py
+++ backup_twitter/save_single_tweet.py
@@ -4,14 +4,20 @@
 Usage: save_single_tweet.py --id=<ID> --dst=<DST>
 """
 
+import os
 import subprocess
+import sys
 
 import docopt
+
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 if __name__ == '__main__':
     args = docopt.docopt(__doc__)
     subprocess.check_call([
-        'python', 'backup_twitter.py', '--credentials=auth.json',
+        sys.executable, os.path.join(SCRIPT_DIR, 'backup_twitter.py'),
+        '--credentials', os.path.join(SCRIPT_DIR, 'auth.json'),
         f'--dir=/Users/alexwlchan/Dropbox/twitter/{args["--dst"]}',
         '--method=statuses_lookup', f'--args=[[{args["--id"]}]]'
     ])
