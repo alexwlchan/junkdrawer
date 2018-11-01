c254c0b70df4de241247a8b4eb198037a6ade7ef
Alex Chan <alex@alexwlchan.net>
2018-11-01 11:17:01 +0000

    Add the other useful snippet here

--- wellcome/transformer_individual_test.scala
+++ wellcome/transformer_individual_test.scala
@@ -8,3 +8,31 @@ it("transforms this record") {
 
   transformToWork(transformable)
 }
+
+// Or if you have the notification message from the SQS Console, you can do
+// the whole thing like this:
+
+it("transforms this record") {
+    import SierraTransformable._
+    import com.amazonaws.services.s3.AmazonS3ClientBuilder
+    import uk.ac.wellcome.messaging.sns.NotificationMessage
+    import uk.ac.wellcome.storage.vhs.HybridRecord
+
+    val queueJsonString = """<...>"""
+
+    val notification: NotificationMessage = fromJson[NotificationMessage](queueJsonString).get
+    val hybridRecord = fromJson[HybridRecord](notification.Message).get
+
+    val s3client = AmazonS3ClientBuilder.standard.withRegion("eu-west-1").build()
+
+    val jsonString =
+      scala.io.Source
+        .fromInputStream(
+          s3client.getObject(hybridRecord.location.namespace, hybridRecord.location.key).getObjectContent
+        )
+        .mkString
+
+    val transformable = fromJson[SierraTransformable](jsonString).get
+
+    transformToWork(transformable)
+  }
