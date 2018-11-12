daeeb54ad2ae5c1a7f2a6ba033fa55a1d1361b40
Alex Chan <a.chan@wellcome.ac.uk>
2018-11-12 14:08:43 +0000

    Delete all this commented-out code

--- aws/list_memory_cpu_of_fargate_tasks.go
+++ aws/list_memory_cpu_of_fargate_tasks.go
@@ -118,42 +118,4 @@ func main() {
             }
         }
     }
-
-    // subscriptionCountsByTopicArn := make(map[string]int)
-    //
-    // listTopicsParams := &sns.ListTopicsInput{}
-    // listTopicsErr := snsClient.ListTopicsPages(
-    //     listTopicsParams,
-    //     func(page *sns.ListTopicsOutput, lastPage bool) bool {
-    //         for _, topic := range page.Topics {
-    //             subscriptionCountsByTopicArn[*topic.TopicArn] = 0
-    //         }
-    //         return true
-    //     })
-    //
-    // if listTopicsErr != nil {
-    //     fmt.Println("Error describing topics: %v", listTopicsErr)
-    //     os.Exit(1)
-    // }
-    //
-    // listSubscriptionParams := &sns.ListSubscriptionsInput{}
-    // listSubscriptionsErr := snsClient.ListSubscriptionsPages(
-    //     listSubscriptionParams,
-    //     func(page *sns.ListSubscriptionsOutput, lastPage bool) bool {
-    //         for _, subscription := range page.Subscriptions {
-    //             subscriptionCountsByTopicArn[*subscription.TopicArn] += 1
-    //         }
-    //         return true
-    //     })
-    //
-    // if listSubscriptionsErr != nil {
-    //     fmt.Println("Error describing subscriptions: %v", listSubscriptionsErr)
-    //     os.Exit(1)
-    // }
-    //
-    // for topicArn, subscriptionCount := range subscriptionCountsByTopicArn {
-    //     if subscriptionCount == 0 {
-    //         fmt.Println(topicArn)
-    //     }
-    // }
 }
