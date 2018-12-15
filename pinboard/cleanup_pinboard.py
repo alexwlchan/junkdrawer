6ef61625f4fdca55f2226a6c42ccc61296c47335
Alex Chan <alex@alexwlchan.net>
2018-12-15 09:15:40 +0000

    Go ahead and actually update the bookmark

--- pinboard/cleanup_pinboard.py
+++ pinboard/cleanup_pinboard.py
@@ -13,20 +13,31 @@ from text_transforms import cleanup_blockquote_whitespace
 
 
 if __name__ == '__main__':
-    # try:
-    #     api_key = sys.argv[1]
-    # except IndexError:
-    #     sys.exit(f"Usage: {__file__} <API_KEY>")
-    #
-    # sess = create_session(api_key)
-    # resp = sess.get("https://api.pinboard.in/v1/posts/all")
-    # print(resp.text)
-    import json; bookmarks = json.load(open("all_bookmarks.json"))
+    try:
+        api_key = sys.argv[1]
+    except IndexError:
+        sys.exit(f"Usage: {__file__} <API_KEY>")
+
+    sess = create_session(api_key)
+    resp = sess.get("https://api.pinboard.in/v1/posts/all")
+
+    bookmarks = resp.json()
 
     for b in bookmarks:
         original_b = copy.deepcopy(b)
 
         b["extended"] = cleanup_blockquote_whitespace(b["extended"])
 
-        print(b)
-        break
+        if b != original_b:
+            print(f"Updating {b['href']}")
+
+            # We need to replace 'href' with 'URL' for the Pinboard API to accept
+            # this as a bookmark.
+            b["url"] = b.pop("href")
+
+            resp = sess.get(
+                "https://api.pinboard.in/v1/posts/add",
+                params=b
+            )
+
+            assert resp.json()["result_code"] == "done", resp.text
