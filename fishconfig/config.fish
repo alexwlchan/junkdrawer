db64f60de16b69281609573d34148415c273e2a4
Alex Chan <alex@alexwlchan.net>
2018-12-24 11:51:24 +0000

    Adjust my "last_screenshot" snippet for macOS Mojave

--- fishconfig/config.fish
+++ fishconfig/config.fish
@@ -42,14 +42,20 @@ function tfdiff
     python3 $ROOT/tfdiff.py (pbpaste)
 end
 
-# Get the last screenshot I took
-alias last_screenshot="find ~/Desktop -name 'Screen Shot*' | grep -v reborder | tail -n 1"
+# Get the last screenshot I took.
+#
+# Note that Mojave changed the format of the filenames: previously they
+# were "Screen Shot <date>", now they're "Screenshot <date>".
+#
+function last_screenshot
+  find ~/Desktop -name 'Screen Shot*' -print0 -o -name 'Screenshot*' -print0 | grep -v 'reborder.png' xargs -0 stat -f '%m %N' | sort -rn | head -1 | cut -f2- -d" "
+end
 
 # Adjust the border on the last screenshot I took
 function reborder_last_screenshot
   pushd ~/Desktop
     python3 $ROOT/reborder.py (basename (last_screenshot)) $argv[1]
-    find ~/Desktop -name 'Screen Shot*reborder.png' | tail -n 1
+    find ~/Desktop -name 'Screen Shot*reborder.png' -print0 -o -name 'Screenshot*reborder.png' -print0 | xargs -0 stat -f '%m %N' | sort -rn | head -1 | cut -f2- -d" "
   popd
 end
 
