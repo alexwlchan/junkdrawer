75414fa5a1edd67ad50f57bdb74f209989baa25b
Alex Chan <a.chan@wellcome.ac.uk>
2019-01-08 14:49:34 +0000

    Use the improved temp credentials script
    as described in https://alexwlchan.net/2018/12/getting-credentials-for-an-assumed-iam-role/

--- aws/issue_temporary_credentials.py
+++ aws/issue_temporary_credentials.py
@@ -9,62 +9,57 @@ import boto3
 import click
 
 
-iam_client = boto3.client("iam")
-sts_client = boto3.client("sts")
+def get_credentials(*, account_id, role_name):
+    iam_client = boto3.client("iam")
+    sts_client = boto3.client("sts")
 
+    username = iam_client.get_user()["User"]["UserName"]
 
-def get_credentials(*, account_id, account_name, role_name):
+    role_arn = f"arn:aws:iam::{account_id}:role/{role_name}"
+    role_session_name = f"{username}@{role_name}.{account_id}"
+
     resp = sts_client.assume_role(
-        RoleArn=f"arn:aws:iam::{account_id}:role/{role_name}",
-        RoleSessionName=f"{role_name}@{account_name}"
+        RoleArn=role_arn,
+        RoleSessionName=role_session_name
     )
 
     return resp["Credentials"]
 
 
-def update_config(*, account_name, credentials):
+def update_credentials_file(*, profile_name, credentials):
     aws_dir = os.path.join(os.environ["HOME"], ".aws")
 
     credentials_path = os.path.join(aws_dir, "credentials")
     config = configparser.ConfigParser()
     config.read(credentials_path)
 
-    if account_name not in config.sections():
-        config.add_section(account_name)
+    if profile_name not in config.sections():
+        config.add_section(profile_name)
 
-    assert account_name in config.sections()
+    assert profile_name in config.sections()
 
-    config[account_name]["aws_access_key_id"] = credentials["AccessKeyId"]
-    config[account_name]["aws_secret_access_key"] = credentials["SecretAccessKey"]
-    config[account_name]["aws_session_token"] = credentials["SessionToken"]
+    config[profile_name]["aws_access_key_id"] = credentials["AccessKeyId"]
+    config[profile_name]["aws_secret_access_key"] = credentials["SecretAccessKey"]
+    config[profile_name]["aws_session_token"] = credentials["SessionToken"]
 
     config.write(open(credentials_path, "w"), space_around_delimiters=False)
 
 
 @click.command()
 @click.option("--account_id", required=True)
-@click.option("--account_name")
 @click.option("--role_name", required=True)
-def assume_role(account_id, account_name, role_name):
-    if account_name is None:
-        account_name = account_id
+@click.option("--profile_name")
+def save_assumed_role_credentials(account_id, role_name, profile_name):
+    if profile_name is None:
+        profile_name = account_id
 
     credentials = get_credentials(
         account_id=account_id,
-        account_name=account_name,
         role_name=role_name
     )
 
-    update_config(account_name=account_name, credentials=credentials)
+    update_credentials_file(profile_name=profile_name, credentials=credentials)
 
 
-if __name__ == '__main__':
-    assume_role()
-#
-# resp = sts_client.assume_role(
-#     RoleArn=sys.argv[1],
-#     RoleSessionName=sys.argv[2]
-# )
-#
-# from pprint import pprint
-# pprint(resp)
+if __name__ == "__main__":
+    save_assumed_role_credentials()
