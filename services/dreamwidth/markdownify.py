0869101c239d871b76882fac53033dc74f9ba9ba
Alex Chan <alex@alexwlchan.net>
2019-01-29 20:51:26 +0000

    Add a quick script for Markdown ~> HTML Dreamwidth entries

---
+++ services/dreamwidth/markdownify.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8
+
+import sys
+
+import markdown2
+
+
+if __name__ == "__main__":
+    try:
+        path = sys.argv[1]
+    except IndexError:
+        sys.exit("Usage: %s <PATH>" % __file__)
+
+    md_src = open(path).read()
+    print("".join(markdown2.markdown(md_src).splitlines()))
