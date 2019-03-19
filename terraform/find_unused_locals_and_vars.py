87f57690969895c0756b9f13ab118a4de9b3b8e8
Alex Chan <a.chan@wellcome.ac.uk>
2019-03-19 10:55:59 +0000

    Add a script for finding unused locals and vars

---
+++ terraform/find_unused_locals_and_vars.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8
+
+import os
+import sys
+
+import hcl
+
+
+rc = 0
+
+for root, _, filenames in os.walk("."):
+    if not any(f.endswith(".tf") for f in filenames):
+        continue
+
+    if ".terraform" in root:
+        continue
+
+    defined_locals = set()
+    defined_variables = set()
+
+    for f in filenames:
+        if not f.endswith(".tf"):
+            continue
+
+        tf_data = hcl.load(open(os.path.join(root, f)))
+        for k in ("module", "resource", "terraform", "provider", "data"):
+            try:
+                del tf_data[k]
+            except KeyError:
+                pass
+
+        defined_locals ^= set(tf_data.get("locals", {}).keys())
+        defined_variables ^= set(tf_data.get("variable", {}).keys())
+
+    unseen_locals = defined_locals.copy()
+    unseen_variables = defined_variables.copy()
+
+    for f in filenames:
+
+        # We've seen everything, so we can skip to the next directory
+        if not unseen_locals and not unseen_variables:
+            break
+
+        if not f.endswith(".tf"):
+            continue
+
+        contents = open(os.path.join(root, f)).read()
+
+        for varname in list(unseen_variables):
+            if "var." + varname in contents:
+                unseen_variables.remove(varname)
+
+        for locname in list(unseen_locals):
+            if "local." + locname in contents:
+                unseen_locals.remove(locname)
+
+    if unseen_locals or unseen_variables:
+        print(f"*** {root}")
+
+        if unseen_locals:
+            print("*** Unused locals:")
+            for locname in unseen_locals:
+                print(f"    - {locname}")
+
+        if unseen_variables:
+            print("*** Unused variables:")
+            for locname in unseen_variables:
+                print(f"    - {locname}")
+
+        print("\n")
+        rc = 1
+
+sys.exit(rc)
