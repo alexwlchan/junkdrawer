4477401391d7cab47d658f38e7896ba6444d0c77
Alex Chan <alex@alexwlchan.net>
2019-07-23 20:25:06 +0100

    Bump the version of the Wellcome release tooling

--- fishconfig/_wellcome.fish
+++ fishconfig/_wellcome.fish
@@ -30,5 +30,5 @@ function wrt
     --volume (git rev-parse --show-toplevel):(git rev-parse --show-toplevel) \
     --workdir (git rev-parse --show-toplevel) \
     --interactive --tty --rm \
-    wellcome/release_tooling:60 $argv
+    wellcome/release_tooling:67 $argv
 end
