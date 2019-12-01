dcbdee352ebfdde4473001ea87694b0f58d6453e
Alex Chan <alex@alexwlchan.net>
2019-12-01 15:06:56 +0000

    Add the GIF slicer script

---
+++ gif_slicer/run.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+
+# Lemon
+python turn_catalogue_image_into_gif.py "https://wellcomecollection.org/works/t2h4c4n3" --rows=4 --columns=6
+
+# Granadilla
+python turn_catalogue_image_into_gif.py "https://wellcomecollection.org/works/h9uqpwcc" --rows=4 --columns=7
+python turn_catalogue_image_into_gif.py "https://wellcomecollection.org/works/vucfggbh" --rows=4 --columns=7
+
+# Persimmon
+python turn_catalogue_image_into_gif.py "https://wellcomecollection.org/works/u6n4x6at" --rows=4 --columns=5
+python turn_catalogue_image_into_gif.py "https://wellcomecollection.org/works/dw58q6nu" --rows=4 --columns=5
+
+# Artichoke
+python turn_catalogue_image_into_gif.py "https://wellcomecollection.org/works/b9g485fs" --rows=4 --columns=7
+python turn_catalogue_image_into_gif.py "https://wellcomecollection.org/works/ffffayxy" --rows=4 --columns=7
+
+# Tomato
+python turn_catalogue_image_into_gif.py "https://wellcomecollection.org/works/nmppywpm" --rows=4 --columns=6
+python turn_catalogue_image_into_gif.py "https://wellcomecollection.org/works/vd6zqnsc" --rows=4 --columns=6
+
+# Guava
+python turn_catalogue_image_into_gif.py "https://wellcomecollection.org/works/fwbqfd89" --rows=4 --columns=6
+python turn_catalogue_image_into_gif.py "https://wellcomecollection.org/works/v9x893kv" --rows=4 --columns=6
+
+# Passion fruit
+python turn_catalogue_image_into_gif.py "https://wellcomecollection.org/works/xkn4pw2u" --rows=4 --columns=6
+
+# Garlic
+python turn_catalogue_image_into_gif.py "https://wellcomecollection.org/works/qbv99v5k" --rows=4 --columns=4
+python turn_catalogue_image_into_gif.py "https://wellcomecollection.org/works/vwp2yvdd" --rows=4 --columns=4
+
+# Kiwi
+python turn_catalogue_image_into_gif.py "https://wellcomecollection.org/works/aqzd3qh7" --rows=4 --columns=7
+python turn_catalogue_image_into_gif.py "https://wellcomecollection.org/works/n8xrxskt" --rows=4 --columns=7
+
+# Star fruit
+python turn_catalogue_image_into_gif.py "https://wellcomecollection.org/works/e9x9hrte" --rows=4 --columns=7
+
+# Pumpkin
+python turn_catalogue_image_into_gif.py "https://wellcomecollection.org/works/rb4sm6fd" --rows=3 --columns=6
+python turn_catalogue_image_into_gif.py "https://wellcomecollection.org/works/z3cja6w4" --rows=3 --columns=6
+
+# Mandarin orange
+python turn_catalogue_image_into_gif.py "https://wellcomecollection.org/works/sxm89b3x" --rows=4 --columns=7
+python turn_catalogue_image_into_gif.py "https://wellcomecollection.org/works/semnhwc6" --rows=4 --columns=7
+
+# Cabbage
+python turn_catalogue_image_into_gif.py "https://wellcomecollection.org/works/rq48tke5" --rows=4 --columns=7
+python turn_catalogue_image_into_gif.py "https://wellcomecollection.org/works/kgz34m4e" --rows=4 --columns=7
+
+# Strawberry
+python turn_catalogue_image_into_gif.py "https://wellcomecollection.org/works/jt4qn2qc" --rows=4 --columns=5
+
+# Apple
+python turn_catalogue_image_into_gif.py "https://wellcomecollection.org/works/mwn9qf6m" --rows=4 --columns=7
+
+# Squash
+python turn_catalogue_image_into_gif.py "https://wellcomecollection.org/works/x6vcmzba" --rows=4 --columns=6
