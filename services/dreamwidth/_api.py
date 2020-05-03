a9e8a3f11c9ec145362a747680e5777b91566908
Alex Chan <alex@alexwlchan.net>
2020-05-03 10:15:32 +0100

    Strip out all the MD5-related login code
    Dreamwidth recently removed the use of MD5 for logins, in favour of
    sending passwords encrypted with HTTPS.  This means I can simplify the
    code that accesses Dreamwidth and the API.
    
    See:
    
    https://dw-dev.dreamwidth.org/221358.html
    https://dw-dev.dreamwidth.org/221167.html

--- services/dreamwidth/_api.py
+++ services/dreamwidth/_api.py
@@ -10,7 +10,6 @@ Useful links:
 
 """
 
-import hashlib
 import json
 
 try:
@@ -18,52 +17,35 @@ try:
 except ImportError:  # Python 3
     from xmlrpc.client import Binary, ServerProxy
 
-import bs4
 import requests
 
 
-def md5(s):
-    h = hashlib.md5()
-    h.update(s.encode("ascii"))
-    return h.hexdigest()
-
-
 class DreamwidthAPI:
-
     def __init__(self, username, password):
         self.username = username
         self.password = password
 
         self.server = ServerProxy("https://www.dreamwidth.org/interface/xmlrpc")
 
-    def auth_info(self):
-        # Invoke LJ.XMLRPC.getchallenge on the endpoint.
-        # See https://dw-dev-training.dreamwidth.org/58924.html?thread=383532
-        challenge_resp = self.server.LJ.XMLRPC.getchallenge()
-        auth_challenge = challenge_resp["challenge"]
-
-        auth_response = md5(auth_challenge + md5(self.password))
-        return {
-            "username": self.username,
-            "auth_method": "challenge",
-            "auth_challenge": auth_challenge,
-            "auth_response": auth_response
-        }
-
     def call_endpoint(self, method_name, data=None):
         if data is None:
             data = {}
-        data.update(self.auth_info())
-        data.update({"ver": "1"})
+
+        # See http://wiki.dwscoalition.org/wiki/index.php/XML-RPC_Protocol_Method:_login
+        data.update(
+            {
+                "username": self.username,
+                "password": self.password,
+                "auth_method": "clear",
+                "ver": "1",
+            }
+        )
 
         method = getattr(self.server, "LJ.XMLRPC." + method_name)
         return method(data)
 
     def get_all_posts(self):
-        data = {
-            "selecttype": "lastn",
-            "howmany": 50
-        }
+        data = {"selecttype": "lastn", "howmany": 50}
 
         # I'm doing my own book-keeping of event IDs to ensure that this function
         # never returns a duplicate item, even if we mess up the API call and
@@ -90,9 +72,7 @@ class DreamwidthAPI:
 
             # This ensures that if there were multiple posts at the same time as
             # the earliest event in the response, we'll get all of them.
-            sorted_logtimes = sorted(
-                set(event["logtime"] for event in resp["events"])
-            )
+            sorted_logtimes = sorted(set(event["logtime"] for event in resp["events"]))
             data["beforedate"] = sorted_logtimes[1]
 
     def get_custom_access_groups(self):
@@ -110,45 +90,15 @@ class DreamwidthSession:
     def __init__(self, username, password):
         self.sess = requests.Session()
 
-        # First we have to log in to Dreamwidth.  This is a bit more complicated
-        # than a standard username/password form.
-        #
-        # Possibly because the LiveJournal/Dreamwidth codebase predates widespread
-        # adoption/support for HTTPS, the login page doesn't just send an encrypted
-        # username/password.  Instead, the login page includes an auth and a challenge.
-        #
-        # The challenge is prepended to an MD5 hash of the password, all of which is
-        # in turn MD5-hashed.  The resulting string is sent to the server, and the
-        # plaintext password omitted.
-        #
-        # You can see the browser-side implementation in question in login.js:
-        # https://github.com/dreamwidth/dw-free/blob/3467339cd823c6d71b8a235bf76409d5dab93b85/htdocs/js/login.js
-        #
-        # This bit definitely works, but it's probably fragile.
-        resp = self.sess.get('https://www.dreamwidth.org/login')
-        login_soup = bs4.BeautifulSoup(resp.text, 'html.parser')
-        lj_form_auth = login_soup.find(
-            'input', attrs={'name': 'lj_form_auth'}).attrs['value']
-        chal = login_soup.find('input', attrs={'name': 'chal'}).attrs['value']
-
-        response_field = md5(chal + md5(password))
-
         resp = self.sess.post(
-            'https://www.dreamwidth.org/login',
-            data={
-                'user': username,
-                'password': '',
-                'lj_form_auth': lj_form_auth,
-                'chal': chal,
-                'action:login': 'Log in',
-                'response': response_field,
-            }
+            "https://www.dreamwidth.org/login",
+            data={"user": username, "password": password, "action:login": "Log in"},
         )
         resp.raise_for_status()
 
         # Dreamwidth always returns an HTTP 200, even if login fails.  This is a
         # better way to check if login succeeded.
-        assert 'Welcome back to Dreamwidth!' in resp.text
+        assert "Welcome back to Dreamwidth!" in resp.text
 
     def get(self, *args, **kwargs):
         return self.sess.get(*args, **kwargs)
