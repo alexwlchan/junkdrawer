9a5f730a3b32f92a5b90c39412b304d1ada01cc2
Alex Chan <alex@alexwlchan.net>
2020-12-06 00:31:49 +0000

    tar up my pinboard backups
    I don't need to access these files very often (if ever), and they
    definitely don't need to be unpacked.  This brings the number of files on
    disk down from ~500k ~> ~10k, which is way easier for all sorts of reasons.

---
+++ backups/pinboard/backup_pinboard.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+
+USERNAME="alexwlchan"
+PASSWORD=$(keyring get pinboard password)
+
+mkdir -p ~/Documents/backups/pinboard
+
+for script in save_bookmarks_list.py save_cache_ids.py save_archive_copies.py save_wget_copies.py
+do
+  python3 "$script" --username="$USERNAME" --password="$PASSWORD"
+done
