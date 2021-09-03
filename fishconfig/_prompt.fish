11e4b1272619934d3493d7adba8181de98fef805
Alex Chan <alex@alexwlchan.net>
2021-09-03 10:21:26 +0100

    Simplify my fish prompt

--- fishconfig/_prompt.fish
+++ fishconfig/_prompt.fish
@@ -19,52 +19,25 @@ function parse_git_branch
 end
 
 
-function _display_hostname
-  if test (sysctl hw.model 2>/dev/null | grep iMac)
-    printf "🖥️ "
-  else if test (sysctl hw.model 2>/dev/null | grep MacBook)
-    printf "💻"
-  else if test (sysctl kernel.osrelease 2>/dev/null | grep linode)
-    printf "☁️ "
-  else
-    printf (hostname)
-  end
-end
-
-
 function fish_prompt
-  # A newline between new prompts for cleanliness
-  echo ''
-
-  # If we're in a virtualenv, print the venv name
-  if set -q VIRTUAL_ENV
-    echo -n -s (set_color yellow) "(" (basename "$VIRTUAL_ENV") ") "
+  # Put a newline between new prompts for cleanliness, but not on the first run.
+  if test -f /tmp/$TERM_SESSION_ID
+    echo ''
   end
 
-  # Print the username
-  set_color magenta
-  printf (eval whoami)
+  touch /tmp/$TERM_SESSION_ID
 
-  # Print the hostname
-  set_color normal
-  printf (echo ' at ')
-  set_color yellow
-  _display_hostname
+  if [ (prompt_pwd) = "~" ]
+    echo '$ '
+    return
+  end
 
   # Print the current directory
-  set_color normal
-  printf (echo ' in ')
   set_color green
   printf (echo -n (prompt_pwd))
 
   # Add information about the current VCS
   parse_git_branch
-
-  if set -q AWS_PROFILE
-    if test -n "$AWS_PROFILE"
-      echo -n -s (set_color yellow) " [👤 $AWS_PROFILE]"
-    end
-  end
 
   # Finally, print the shell prompt.  We have a slightly different prompt
   # for root users.
