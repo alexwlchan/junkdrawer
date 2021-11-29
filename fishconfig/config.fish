e9072da50e0a48d03f6d4186479e83ffd01accf2
Alex Chan <alex@alexwlchan.net>
2021-11-29 01:04:13 +0000

    add some terraform bits

--- fishconfig/config.fish
+++ fishconfig/config.fish
@@ -34,6 +34,12 @@ function tmpdir
     cd (mktemp -d)
 end
 
+function scratch
+    set DIR ~/"Desktop/scratch/scratch."(date +"%Y-%m-%d")"."(openssl rand -hex 4)
+    mkdir -p "$DIR"
+    cd "$DIR"
+end
+
 # Alias for finding out which subdirectories of the current dir contain
 # the most files.  Useful when trying to find wasted disk space.
 alias cdir 'for l in (ls); if [ -d $l ]; echo (find $l | wc -l)"  $l"; end; end | sort'
@@ -47,6 +53,21 @@ end
 # See https://www.terraform.io/docs/configuration/providers.html#provider-plugin-cache
 set -x TF_PLUGIN_CACHE_DIR ~/.terraform.d/plugin-cache
 
+function tfi
+  if test -f run_terraform.sh
+    ./run_terraform.sh init
+  else
+    terraform init
+  end
+end
+
+function tfa
+  if test -f run_terraform.sh
+    ./run_terraform.sh apply terraform.plan
+  else
+    terraform apply terraform.plan
+  end
+end
 
 ###############################################################################
 # Other fish config files
@@ -67,3 +88,6 @@ end
 if [ (hostname) = "WTC02DT99KML7H" ]
 	bash $DIR/pathscripts/killall_nomad.sh &
 end
+
+alias imgcat=$HOME/.iterm2/imgcat;alias imgls=$HOME/.iterm2/imgls;alias it2api=$HOME/.iterm2/it2api;alias it2attention=$HOME/.iterm2/it2attention;alias it2check=$HOME/.iterm2/it2check;alias it2copy=$HOME/.iterm2/it2copy;alias it2dl=$HOME/.iterm2/it2dl;alias it2getvar=$HOME/.iterm2/it2getvar;alias it2git=$HOME/.iterm2/it2git;alias it2setcolor=$HOME/.iterm2/it2setcolor;alias it2setkeylabel=$HOME/.iterm2/it2setkeylabel;alias it2ul=$HOME/.iterm2/it2ul;alias it2universion=$HOME/.iterm2/it2universion
+
